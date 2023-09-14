73cdfbff35377f94249b27664770dfd855fb280d
phucdee <120212dinhphuc@isneu.org>
2023-09-15 00:21:49 +0700

    Fix bug in Unity Ads, remove stream_check available

--- airbyte-integrations/connectors/source-unity-ads-advertise/source_unity_ads_advertise/source.py
+++ airbyte-integrations/connectors/source-unity-ads-advertise/source_unity_ads_advertise/source.py
@@ -8,6 +8,7 @@ from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
 from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams.availability_strategy import AvailabilityStrategy
 from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
@@ -23,6 +24,10 @@ class UnityAdsAdvertiseStream(HttpStream, ABC):
     def __init__(self,config:Mapping[str,any] ,*arg,**kwargs):
         super().__init__(*arg, **kwargs)
         self.config = config
+    
+    @property
+    def availability_strategy(self) -> Optional["AvailabilityStrategy"]:
+        return None
 
     @property
     def http_method(self) -> str:
@@ -88,9 +93,12 @@ class UnityAdsAdvertiseCheckConnectionStream(UnityAdsAdvertiseStream):
 
 class UnityAdsAdvertiseIncrementalStream(UnityAdsAdvertiseStream, IncrementalMixin):
     primary_key = None
-    number_days_backward_default = 7
-    _record_date_format = "YYYY-MM-DDTHH:mm:ss.SSS[Z]"
-    _cursor_value: datetime = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._cursor_value = None
+        self.number_days_backward = self.config.get("number_days_backward", 7)
+        self.timezone  = self.config.get("timezone", "UTC")
 
     def path(self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> str:
@@ -150,54 +158,35 @@ class UnityAdsAdvertiseIncrementalStream(UnityAdsAdvertiseStream, IncrementalMix
     
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
         slice: list = []
+
+        # data_available_date is the date that the newest data can be accessed
+        data_avaliable_date : datetime.date = pendulum.today(self.timezone).date()
         
-        number_days_backward: int = int(next(filter(None,[self.config.get('number_days_backward')]),self.number_days_backward_default))
+        # if stream has stream_state which means it has been run before, so start_date will be subtract X days backwards from last time run
         if stream_state:
-            ''' this code for incremental run, the stream will start with the last date of record minus number_days_backward'''
-            start_date: datetime.date = self.state[self.cursor_field].subtract(days=number_days_backward)
-            # self.logger.info(f"stream slice start date in IF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
-            start_date_as_str: str = start_date.to_date_string()
-            today_as_string: str = pendulum.today().to_date_string()
-            slice.append({
-                "start": start_date_as_str,
-                "end": today_as_string
-                }
-            )
-
-        elif self._cursor_value: 
-            '''' this code for the first time run or full refresh run, the stream will start with the start date in config'''
-            start_date: datetime.date = pendulum.parse(self.config["start_date"]).date()
-            # self.logger.info(f"stream slice start date in ELIF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
-
-            while start_date <= pendulum.today().date():
-                start_date_as_str: str = start_date.to_date_string()
-                if start_date.month == pendulum.today().month:
-                    today_as_string: str = pendulum.today().to_date_string()
-                    slice.append({
-                        "start": start_date_as_str,
-                        "end": today_as_string
-                        }
-                    )
-                else:
-                    end_date_as_str: str = start_date.end_of('month').to_date_string()
-                    slice.append({
-                        "start": start_date_as_str,
-                        "end": end_date_as_str
-                        }
-                    )
-                start_date: datetime.date = start_date.add(months=1).start_of('month')
-
+            # print(f' stream slice, stream state in IF {stream_state}, {self._cursor_value}')
+            start_date: datetime.date = self.state[self.cursor_field].subtract(days=self.number_days_backward)
         else:
-            ''' this code for airbyte to checking stream availability. It will be run first then starting sync. In order to make this procees shorter, start date is yesteray and end date is today'''  
-            start_date:datetime.date = pendulum.today().subtract(days=1).date()
-            # self.logger.info(f"stream slice start date in ELSE {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
+            # print(f' stream slice, stream state in ELSE {stream_state}, {self._cursor_value}')
+            start_date: datetime.date = pendulum.parse(self.config["start_date"]).date()
+
+        while start_date <= data_avaliable_date:
             start_date_as_str: str = start_date.to_date_string()
-            today_as_string: str = pendulum.today().to_date_string()
-            slice.append({
-                "start": start_date_as_str,
-                "end": today_as_string
-                }
-            )
+            if start_date.month == data_avaliable_date.month:
+                end_date_as_str: str = data_avaliable_date.to_date_string()
+                slice.append({
+                    "start": start_date_as_str,
+                    "end": end_date_as_str
+                    }
+                )
+            else:
+                end_date_as_str: str = start_date.end_of('month').to_date_string()
+                slice.append({
+                    "start": start_date_as_str,
+                    "end": end_date_as_str
+                    }
+                )
+            start_date: datetime.date = start_date.add(months=1).start_of('month')
 
         return slice or [None]
     
@@ -229,10 +218,6 @@ class UnityAdsAdvertiseIncrementalStream(UnityAdsAdvertiseStream, IncrementalMix
             record_cursor_value: datetime.date = pendulum.parse(record[self.cursor_field]).date()
             self._cursor_value: datetime.date = max(self._cursor_value, record_cursor_value) if self._cursor_value else record_cursor_value
             yield record
-        
-        # if there is no record backs, the cursor value will be None, so we update it as the start date in config
-        if self._cursor_value == None:
-                self._cursor_value: datetime.date = pendulum.parse((self.config["start_date"])).date()
     
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response_as_string: str = response.content.decode('utf-8-sig')
