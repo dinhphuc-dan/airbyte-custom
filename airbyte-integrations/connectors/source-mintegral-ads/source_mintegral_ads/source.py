c9863f943efbb87148c9101fe5a5212f4ef03b28
phucdinh <phucdm@volio.vn>
2023-10-30 15:43:19 +0700

    complete Mintegral Ads

--- airbyte-integrations/connectors/source-mintegral-ads/source_mintegral_ads/source.py
+++ airbyte-integrations/connectors/source-mintegral-ads/source_mintegral_ads/source.py
@@ -53,7 +53,7 @@ class MintegralAdsStream(HttpStream, ABC):
 
 
 class MintegralCheckConnection(MintegralAdsStream):
-    max_limit_per_page = 2
+    max_limit_per_page = 50
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -80,21 +80,21 @@ class MintegralCheckConnection(MintegralAdsStream):
         if next_page_token:
             next_page = next_page_token + 1
             params.update({"page": next_page})
-        print(f"request params AFTER IF, params as {params}")
+        self.logger.info(f"check campaign, params as  {params}")
         return params
     
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        list_campaign_as_dict = {}
         response_json = response.json()
         for record in response_json['data']['list']:
+            list_campaign_as_dict = {}
             list_campaign_as_dict.update({'campaign_id': record['campaign_id'], 'campaign_name': record['campaign_name']})
             yield list_campaign_as_dict
 
 class MintegralAdsReport(MintegralAdsStream, IncrementalMixin):
-    chunk_date_range = 6
+    chunk_date_range = 7
     def __init__(self,list_campaign_as_dict,**kwargs):
         super().__init__(**kwargs)
-        self._list_campaign_as_dict: dict = list_campaign_as_dict
+        self._list_campaign_as_dict: list = list_campaign_as_dict
         self._cursor_value = None
         self.number_days_backward: int = self.config.get("number_days_backward", 7)
         self.timezone: str  = self.config.get("timezone", "UTC")
@@ -140,42 +140,49 @@ class MintegralAdsReport(MintegralAdsStream, IncrementalMixin):
             '''' this code for the first time run or full refresh run, the stream will start with the start date in config'''
             start_date: datetime.date = pendulum.parse(self.config["start_date"]).date()
             # self.logger.info(f"stream slice start date in ELIF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
-
+        
         while start_date <= data_avaliable_date:
             start_date_as_str: str = start_date.to_date_string()
             if (data_avaliable_date - start_date).days >= self.chunk_date_range:
                 end_date: datetime.date = start_date.add(days=self.chunk_date_range)
                 end_date_as_str: str = end_date.to_date_string()
-                slice.append({
-                    'start_date': start_date_as_str,
-                    'end_date': end_date_as_str,
-                })
+                for campaign in self._list_campaign_as_dict:
+                    slice.append({
+                        'start_date': start_date_as_str,
+                        'end_date': end_date_as_str,
+                        "campaign_id": str(campaign['campaign_id']),
+                        "campaign_name": str(campaign['campaign_name']),
+
+                    })
             else:
                 end_date: datetime.date = data_avaliable_date
                 end_date_as_str: str = end_date.to_date_string()
-                slice.append({
-                    'start_date': start_date_as_str,
-                    'end_date': end_date_as_str,
-                })
+                for campaign in self._list_campaign_as_dict:
+                    slice.append({
+                        'start_date': start_date_as_str,
+                        'end_date': end_date_as_str,
+                        "campaign_id": str(campaign['campaign_id']),
+                        "campaign_name": str(campaign['campaign_name']),
+                    })
             start_date: datetime.date = end_date.add(days=1)
-
+        
+        # self.logger.info(f"slice {slice}")
         return slice or [None]
     
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         request_params = {}
-        for campaign in self._list_campaign_as_dict:
-            request_params.update(stream_slice)
-            request_params.update({
-                "utc":self.utc_offset,
-                "dimension": "location",
-                "campaign_id": str(campaign['campaign_id']),
-                "not_empty_field": "click,install,impression,spend"
-            })
-            request_params.update({"format":"json"})
-            self.logger.info(f" stream slice date {stream_slice} for campaign_id {campaign['campaign_id']}")
-            return request_params
+        request_params.update({
+            'start_date': stream_slice['start_date'],
+            'end_date': stream_slice['end_date'],
+            "campaign_id": stream_slice['campaign_id'],
+            "utc":self.utc_offset,
+            "dimension": "location",
+            "not_empty_field": "click,install,impression,spend",
+        })
+        self.logger.info(f" Request Params {stream_slice['start_date'], stream_slice['end_date'], stream_slice['campaign_id']}")
+        return request_params
     
     def read_records(
         self,
@@ -188,17 +195,37 @@ class MintegralAdsReport(MintegralAdsStream, IncrementalMixin):
             return []
         records = super().read_records(sync_mode=sync_mode, cursor_field=cursor_field, stream_slice=stream_slice, stream_state=stream_state)
         for record in records:
-            # record_cursor_value = pendulum.parse(record[self.cursor_field]).date()
-            # self._cursor_value = max(self._cursor_value, record_cursor_value) if self._cursor_value else record_cursor_value
+            record_cursor_value = pendulum.parse(record[self.cursor_field]).date()
+            self._cursor_value = max(self._cursor_value, record_cursor_value) if self._cursor_value else record_cursor_value
             # self.logger.info(f"read record with ELSE, record_cursor_value: {record_cursor_value} and self._cursor_value: {self._cursor_value} ")
+            if record is not None:
+                record.update({'campaign_id': stream_slice['campaign_id']})
+                record.update({'campaign_name': stream_slice['campaign_name']})
             yield record
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         response_json = response.json()
-        yield response_json
-        # results = response_json.get('results')
-        # for record in results:
-        #     yield record
+        # yield response_json
+
+        if response_json['data'] is None:
+            yield {}
+        else:
+            for item in response_json['data']:
+                record = {}
+                record.update({'date': item['date']})
+                record.update({'package_name': item['package_name']})
+                record.update({'platform': item['platform']})
+                record.update({'location': item['location']})
+                record.update({'geo': item['geo']})
+                record.update({'currency': item['currency']})
+                record.update({'utc': item['utc']})
+                record.update({'offer_id': item['offer_id']})
+                record.update({'offer_name': item['offer_name']})
+                record.update({'impression': item['impression']})
+                record.update({'click': item['click']})
+                record.update({'install': item['install']})
+                record.update({'spend': item['spend']})
+                yield record
     
     def get_json_schema(self) -> Mapping[str, Any]:
         full_schema = {
@@ -231,13 +258,15 @@ class SourceMintegralAds(AbstractSource):
         auth = MintegralTokenAuthenticator(config=config)
         campaign_list_generator = MintegralCheckConnection(authenticator=auth,config=config)
         read_records_generator = campaign_list_generator.read_records(sync_mode="full_refresh")
-        # yield from read_records_generator generate each campaign from parse response function
-        yield from read_records_generator
+        list_campaign_as_dict = []
+        list_campaign_as_dict.extend(read_records_generator)
+        return list_campaign_as_dict
+
 
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         try:
             check_campaign_list = self._get_campaign_list(config)
-            if next(check_campaign_list) is not None:
+            if check_campaign_list:
                 return True, None
         except Exception as e:
             return False,
