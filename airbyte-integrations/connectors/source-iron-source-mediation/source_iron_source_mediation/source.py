2639c6c70fcf80bec2213601b136dd16a7c76fa2
phucdinh <phucdm@volio.vn>
2024-05-02 10:07:31 +0700

    upgrade iron source mediation with get last x days

--- airbyte-integrations/connectors/source-iron-source-mediation/source_iron_source_mediation/source.py
+++ airbyte-integrations/connectors/source-iron-source-mediation/source_iron_source_mediation/source.py
@@ -135,20 +135,20 @@ class IronSourceMediationAdSourceReport(IronSourceMediationStream, IncrementalMi
         # data_available_date is the date that the newest data can be accessed
         data_avaliable_date : datetime.date = pendulum.today(self.timezone).date() 
 
-        if stream_state:
-            ''' this code for incremental run, the stream will start with the last date of record minus number_days_backward'''
-            start_date: datetime.date = self.state[self.cursor_field].subtract(days=self.number_days_backward)
+        if self.get_last_X_days:
+            '''' this code for all kind of run, such as: the first time run or full refresh or incremental run, the stream will start with today date minus number_days_backward'''
+            start_date: datetime.date = pendulum.today(self.timezone).subtract(days=self.number_days_backward).date()
             # self.logger.info(f"stream slice start date in IF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
 
-        elif self.get_last_X_days:
-            '''' this code for full refresh run and get_last_X_days is true, the stream will start with today date minus number_days_backward'''
-            start_date: datetime.date = pendulum.today(self.timezone).subtract(days=self.number_days_backward).date()
+        elif stream_state:
+            ''' this code for incremental run and get_last_X_days is false, the stream will start with the last date of stream state minus number_days_backward'''
+            start_date: datetime.date = self.state[self.cursor_field].subtract(days=self.number_days_backward)
             # self.logger.info(f"stream slice start date in ELIF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
 
         else: 
             '''' this code for the first time run or full refresh run, the stream will start with the start date in config'''
             start_date: datetime.date = pendulum.parse(self.config["start_date"]).date()
-            # self.logger.info(f"stream slice start date in ELIF {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
+            # self.logger.info(f"stream slice start date in ELSE {start_date}, cusor value {self._cursor_value}, stream state {stream_state}")
 
         while start_date <= data_avaliable_date:
             start_date_as_str: str = start_date.to_date_string()
@@ -203,6 +203,7 @@ class IronSourceMediationAdSourceReport(IronSourceMediationStream, IncrementalMi
         
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        self.logger.info(f"Status code in Parse Response {response.status_code}")
         response_json = response.json()
         record = {}
         for row in response_json:
